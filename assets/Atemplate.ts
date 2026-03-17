aeab9a879d60e406942b82367c7cd7d1b24d8a46
Jason Chen <warmfulfate@gmail.com>
2026-03-17 22:33:38 +0800

    创建初始规则模板

---
+++ assets/Atemplate.ts
@@ -0,0 +1,16 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'App_PackageName',
+  name: 'APP_Name',
+  groups: [
+    {
+      key: 0,
+      name: '',
+      desc: '',
+      exampleUrls: '', //如果有，可以填入。
+      activityIds: [''],
+      rules: [{}],
+    },
+  ],
+});
